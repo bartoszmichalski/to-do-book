91199fd5e94be3617da7c18cc026caa7cde24d8e
Bartosz Michalski <bartoszmichalski@poczta.fm>
2017-05-25 20:04:27 +0200

    added id of task to td tag

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function () {
             tasks.forEach(function(task){
                 var creationDate = new Date(task.creationDate * 1000);
                 var completionDate = new Date(task.completionDate * 1000);
-                var newLi = jQuery('<tr><td>'+task.description+'</td><td>'+ creationDate.toLocaleDateString('pl-PL')+'</td><td>'+ completionDate.toLocaleDateString('pl-PL')+'</td></tr>');
+                var newLi = jQuery('<tr id='+task.id+'><td>'+task.description+'</td><td>'+ creationDate.toLocaleDateString('pl-PL')+'</td><td>'+ completionDate.toLocaleDateString('pl-PL')+'</td></tr>');
                 newLi.appendTo(tbody);
             });
         }
